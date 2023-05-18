b04d35e1720d357e26ae2b9b949b48a6ba57e754
DayTroy <shadowyguy5@gmail.com>
2023-05-18 17:50:43 +0300

    Added task4

---
+++ task4.py
@@ -0,0 +1,14 @@
+def main(n):
+    if n == 0:
+        return -0.24
+    if n == 1:
+        return 0.60
+    if n >= 2:
+        return 15 * main(n - 2)**3 + 0.02 + main(n - 1)
+
+
+print(main(6))
+print(main(4))
+print(main(3))
+print(main(7))
+print(main(5))
