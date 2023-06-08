016e74ad11276be20c0862705d7c755a7a69d6e6
DayTroy <70051506+DayTroy@users.noreply.github.com>
2023-06-08 22:09:01 +0300

    Update task7.py

--- task7.py
+++ task7.py
@@ -3,11 +3,11 @@ def main(whole):
     mask_k2 = 0b000000000000000000110000
     mask_k3 = 0b000000000000011111000000
     mask_k4 = 0b111111111111100000000000
-    k1 = (whole & mask_k1)
+    k1 = whole & mask_k1
     k2 = (whole & mask_k2) >> 4
     k3 = (whole & mask_k3) >> 6
     k4 = (whole & mask_k4) >> 11
-    return [('K1', k1), ('K2', k2), ('K3', k3), ('K4', k4)]
+    return [("K1", k1), ("K2", k2), ("K3", k3), ("K4", k4)]
 
 
 print(main(299830))
