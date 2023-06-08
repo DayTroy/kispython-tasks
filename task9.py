e4719d7e5918ba394e901409522c0d8a7babf052
DayTroy <70051506+DayTroy@users.noreply.github.com>
2023-06-08 22:10:08 +0300

    Update task9.py

--- task9.py
+++ task9.py
@@ -21,16 +21,63 @@ def main(input_table):
         if row[2]:
             row[2] = re.sub(r"\D", "", row[2])[1:]
         if row[3]:
-            row[3] = re.search(r'\[at](.+)$', row[3]).group(1)
+            row[3] = re.search(r"\[at](.+)$", row[3]).group(1)
     return new_table
 
 
-print(main([["2004/04/13", None, "Вячеслав Г. Цебакянц", None, "+7 (691) 892-54-64", "vaceslav63[at]yahoo.com"],
-            ["2003/02/17", None, "Руслан И. Фушачак", None, "+7 (051) 566-56-18", "fusacak98[at]gmail.com"],
-            ["2004/05/16", None, "Макар Ш. Бумивянц", None, "+7 (734) 291-76-87", "bumivanz16[at]mail.ru"],
-            ["2004/05/16", None, "Макар Ш. Бумивянц", None, "+7 (734) 291-76-87", "bumivanz16[at]mail.ru"],
+print(
+    main(
+        [
+            [
+                "2004/04/13",
+                None,
+                "Вячеслав Г. Цебакянц",
+                None,
+                "+7 (691) 892-54-64",
+                "vaceslav63[at]yahoo.com",
+            ],
+            [
+                "2003/02/17",
+                None,
+                "Руслан И. Фушачак",
+                None,
+                "+7 (051) 566-56-18",
+                "fusacak98[at]gmail.com",
+            ],
+            [
+                "2004/05/16",
+                None,
+                "Макар Ш. Бумивянц",
+                None,
+                "+7 (734) 291-76-87",
+                "bumivanz16[at]mail.ru",
+            ],
+            [
+                "2004/05/16",
+                None,
+                "Макар Ш. Бумивянц",
+                None,
+                "+7 (734) 291-76-87",
+                "bumivanz16[at]mail.ru",
+            ],
             [],
             [],
-            ["2004/05/16", None, "Макар Ш. Бумивянц", None, "+7 (734) 291-76-87", "bumivanz16[at]mail.ru"],
-            ["1999/03/18", None, "Валерий Г. Кидук", None, "+7 (481) 922-22-43", "valerij85[at]mail.ru"],
-            ]))
+            [
+                "2004/05/16",
+                None,
+                "Макар Ш. Бумивянц",
+                None,
+                "+7 (734) 291-76-87",
+                "bumivanz16[at]mail.ru",
+            ],
+            [
+                "1999/03/18",
+                None,
+                "Валерий Г. Кидук",
+                None,
+                "+7 (481) 922-22-43",
+                "valerij85[at]mail.ru",
+            ],
+        ]
+    )
+)
